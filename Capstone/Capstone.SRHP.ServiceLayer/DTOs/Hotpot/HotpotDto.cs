7502a030f802441781a19404b945fe6515cfae80
Pham Duc Tai <149134916+dtai-coding@users.noreply.github.com>
2025-03-12 00:35:33 +0700

    update hotpot(improve inventory, remove types)

--- Capstone/Capstone.SRHP.ServiceLayer/DTOs/Hotpot/HotpotDto.cs
+++ Capstone/Capstone.SRHP.ServiceLayer/DTOs/Hotpot/HotpotDto.cs
@@ -13,16 +13,20 @@ namespace Capstone.HPTY.ServiceLayer.DTOs.Hotpot
         public string Name { get; set; }
         public string Material { get; set; }
         public string Size { get; set; }
-        public string? Description { get; set; }
-        public string[]? ImageURLs { get; set; }
+        public string Description { get; set; }
+        public string[] ImageURLs { get; set; }
         public decimal Price { get; set; }
         public decimal BasePrice { get; set; }
-        public decimal DepositAmount { get; set; } // 70% of BasePrice
         public bool Status { get; set; }
         public int Quantity { get; set; }
         public DateTime LastMaintainDate { get; set; }
-        public HotpotTypeDto? HotpotType { get; set; }
+        public int InventoryID { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
+    }
+
+    public class HotpotDetailDto : HotpotDto
+    {
+        public List<InventoryItemDto> InventoryItems { get; set; }
     }
 }
