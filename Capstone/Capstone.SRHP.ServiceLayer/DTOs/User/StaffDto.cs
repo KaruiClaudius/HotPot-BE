0fa68323705bf9d09fe7fa698a97e0c29ccc6cd1
KaruiClaudius <ckodanh123@gmail.com>
2025-03-12 13:01:51 +0700

    add staff controller and tweak Dto

--- Capstone/Capstone.SRHP.ServiceLayer/DTOs/User/StaffDto.cs
+++ Capstone/Capstone.SRHP.ServiceLayer/DTOs/User/StaffDto.cs
@@ -9,7 +9,10 @@ namespace Capstone.HPTY.ServiceLayer.DTOs.User
 {
     public class StaffDto
     {
-        public int Id { get; set; }
+        public int StaffId { get; set; }
+
+        public int WorkDays { get; set; }
+
         public UserDto User { get; set; } = null!;
         public ICollection<WorkShift> WorkShifts { get; set; } = new List<WorkShift>();
         public ICollection<ShippingOrder> ShippingOrders { get; set; } = new List<ShippingOrder>();
