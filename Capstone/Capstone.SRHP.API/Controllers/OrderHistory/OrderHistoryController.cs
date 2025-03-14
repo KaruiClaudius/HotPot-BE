76a0d2205ef644d7ab8d87e2d6b8471c838cd481
KaruiClaudius <ckodanh123@gmail.com>
2025-03-14 23:56:39 +0700

    up

--- Capstone/Capstone.SRHP.API/Controllers/OrderHistory/OrderHistoryController.cs
+++ Capstone/Capstone.SRHP.API/Controllers/OrderHistory/OrderHistoryController.cs
@@ -25,7 +25,6 @@ namespace Capstone.HPTY.API.Controllers.OrderHistory
             _orderHistoryService = orderHistoryService;
             _logger = logger;
         }
-
         /// <summary>
         /// Get order history with optional filtering
         /// </summary>
