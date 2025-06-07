df68a39129464b0401a59036acf2855d609bf121
Pham Duc Tai <149134916+dtai-coding@users.noreply.github.com>
2025-06-07 20:41:50 +0700

    Update ComboService.cs

--- Capstone/Capstone.SRHP.ServiceLayer/Services/IngredientService/ComboService.cs
+++ Capstone/Capstone.SRHP.ServiceLayer/Services/IngredientService/ComboService.cs
@@ -567,7 +567,7 @@ namespace Capstone.HPTY.ServiceLayer.Services.IngredientService
 
             // Check total quantity of ingredients
             int totalQuantity = comboIngredients.Sum(ci => ci.Quantity);
-            int minTotalQuantity = comboSize * 3; // 3 packages per person
+            int minTotalQuantity = comboSize * 2; 
             if (totalQuantity < minTotalQuantity)
             {
                 throw new ValidationException($"Tổng số lượng gói nguyên liệu ({totalQuantity}) không đủ cho combo kích thước {comboSize}. Cần ít nhất {minTotalQuantity} gói");
